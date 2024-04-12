353a67daae1dd31732c86f9475af043797814251
DiegoDSlo <diego.slowing@gmail.com>
2024-04-11 21:10:09 -0600

    feat: table cards

--- src/MainPage/Restaurante.jsx
+++ src/MainPage/Restaurante.jsx
@@ -4,6 +4,7 @@ import './Box.css'
 import HeaderMain from './Header'
 import NavBar from './Nav'
 import Areas from './Areas'
+import Mesa from './mesa'
 
 
 function Restaurante() {
@@ -19,6 +20,15 @@ function Restaurante() {
                 Mesas
               </header>
               <Areas></Areas>
+              <ul class='mesacontainer'>
+                <Mesa num='01' available={true}></Mesa>
+                <Mesa num='02' available={true}></Mesa>
+                <Mesa num='03' available={false}></Mesa>
+                <Mesa num='04' available={true}></Mesa>
+                <Mesa num='05' available={false}></Mesa>
+                <Mesa num='06' available={true}></Mesa>
+                
+              </ul>
             </div>
           </section>
             
