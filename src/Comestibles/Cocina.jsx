bc47bdb2bec4634c9b2760c2d19ffe81cdf3566e
Fabiola-cc <fcontrerascol@gmail.com>
2024-04-12 10:59:22 -0600

    feat: addition of kitchen and bar pages

---
+++ src/Comestibles/Cocina.jsx
@@ -0,0 +1,29 @@
+import '/src/MainPage/Main.css'
+import '/src/MainPage/Section.css'
+import '/src/MainPage/Box.css'
+import './consumibles.css'
+import HeaderMain from '/src/MainPage/Header'
+import NavBar from '/src/MainPage/Nav'
+import Cards from './comidas'
+
+
+function Cocina() {
+
+  return (
+    <>
+        <main class='main-content'>
+          <HeaderMain></HeaderMain>
+          <NavBar></NavBar>
+          <section>
+            <div class='cardbox'>
+              <header class='headerbox'>Pedidos a trabajar</header>
+              <Cards />
+            </div>
+          </section>
+            
+        </main>
+    </>
+  )
+}
+
+export default Cocina
