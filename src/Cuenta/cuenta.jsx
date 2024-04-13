0c74bca019dde10b2872bc69a68cf870910e9b75
DiegoDSlo <diego.slowing@gmail.com>
2024-04-12 18:14:46 -0600

    feat: cuenta platillos boxes

--- src/Cuenta/cuenta.jsx
+++ src/Cuenta/cuenta.jsx
@@ -2,12 +2,20 @@ import React, { useState } from 'react';
 import '/src/MainPage/Main.css'
 import '/src/MainPage/nav.css'
 import '/src/Mesas/Box.css'
+import './cuenta.css'
 
 function Cuenta(){
     
     return(
         <>
-            <h1>Hola</h1>
+            <div class='cardboxcuenta'>
+                <div class= 'sectioncomida'>
+                    <h3 class = 'tipopedido'>Platillos</h3>
+                    <div class = 'platos'></div>
+                    <h3 class = 'tipopedido'>Bebidas</h3>
+                    <div class = 'platos'></div>
+                </div>
+            </div>
         </>
     )
 
