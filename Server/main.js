6da569bc22077a11365009d3e25aeae5fc439c4f
DiegoDuaS <dua22075@uvg.edu.gt>
2024-04-14 13:06:44 -0600

    feat: connection with data base

--- Server/main.js
+++ Server/main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./conn');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
     try {
