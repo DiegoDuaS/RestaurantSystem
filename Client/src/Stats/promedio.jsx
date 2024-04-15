fdaca16814e9e0eb1a940c7eafc39821f9cba685
Maria-Villafuerte <100331510+Maria-Villafuerte@users.noreply.github.com>
2024-04-14 18:45:11 -0600

    feat: add box to stats

--- Client/src/Stats/promedio.jsx
+++ Client/src/Stats/promedio.jsx
@@ -4,6 +4,10 @@ function PromedioComidas() {
     return (
         <div className="promedio-comidas">
             <h2>Promedio de tiempo que se tardan los clientes en comer</h2>
+            <div className='box_stats1'>
+                <div className='box_stats'>aaa</div>
+            </div>
+            
         </div>
     );
 }
--- Client/src/Stats/quejasPersona.jsx
+++ Client/src/Stats/quejasPersona.jsx
@@ -5,6 +5,10 @@ function QuejasEmpleados() {
     return (
         <div className="quejas-empleados">
             <h2>Reporte de quejas agrupadas por persona</h2>
+            <div className='box_stats1'>
+                <div className='box_stats'>aaa</div>
+            </div>
+            
         </div>
     );
 }
